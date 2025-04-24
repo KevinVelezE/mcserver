25f217bf0a1157f6ab2bc16373a0b35f91b275b8
Kevin Velez <velez.kevin@correounivalle.edu.co>
2025-04-24 11:46:14 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-MC_VERSION="1.21.5"
+MC_VERSION="1.20.1"
 JAR_URL="https://api.papermc.io/v2/projects/paper/versions/${MC_VERSION}/builds/103/downloads/paper-${MC_VERSION}-103.jar"
 JAR_FILE="paper.jar"
 
@@ -14,17 +14,19 @@ fi
 # Aceptar EULA
 echo "eula=true" > eula.txt
 
-# Inicia ngrok en segundo plano
+# Iniciar ngrok con authtoken
 echo "Iniciando ngrok..."
 ngrok config add-authtoken 2jyLerkSdBxERZiDDQto9fpAfUa_2kcvKmFM9Qa5PtKj84RGN
-nohup ngrok tcp 25565 --log=stdout > ngrok.log &
 
-# Esperar unos segundos para que ngrok levante
+# Ejecutar ngrok en background y guardar PID
+nohup ngrok tcp 25565 > ngrok.out &
+
+# Esperar a que ngrok genere la URL
 sleep 5
 
-# Mostrar dirección de ngrok
-echo "Dirección ngrok (puerto TCP):"
-grep -oE "tcp://[0-9a-zA-Z\.]+:[0-9]+" ngrok.log
+# Mostrar dirección ngrok
+echo "🔌 Dirección pública de tu servidor (copia y pega en Minecraft):"
+grep -oE "tcp://[0-9a-zA-Z\.]+:[0-9]+" ngrok.out
 
-# Ejecutar servidor
+# Iniciar servidor Minecraft
 java -Xmx2G -Xms1G -jar $JAR_FILE nogui
